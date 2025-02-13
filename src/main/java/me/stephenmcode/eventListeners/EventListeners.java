b20070d630c39daf564d65d8e626a1f4fcd0ee1a
Stephen <93400341+StephenMCode@users.noreply.github.com>
2025-02-13 09:26:19 -0800

    Messing with simple event listeners

--- src/main/java/me/stephenmcode/eventListeners/EventListeners.java
+++ src/main/java/me/stephenmcode/eventListeners/EventListeners.java
@@ -1,9 +1,14 @@
 package me.stephenmcode.eventListeners;
 
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.kyori.adventure.text.Component;
 
 public final class EventListeners extends JavaPlugin implements Listener {
 
@@ -19,7 +24,24 @@ public final class EventListeners extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
 
-        System.out.println("A player has joined the server!");
+        event.joinMessage(Component.text("Welcome to the server silly goose!"));
+
+    }
+
+    @EventHandler
+    public void onLeaveBed(PlayerBedLeaveEvent event) {
+
+        Player player = event.getPlayer();
+        World world = player.getWorld();
+
+        long timeInTicks = world.getTime();
+
+        if (timeInTicks > 12542 && timeInTicks < 23459) {
+            player.sendMessage("You got out of bed after curfew! You have been punished for your actions.");
+            player.setHealth(0);
+        } else {
+            player.sendMessage("Good morning!");
+        }
 
     }
 

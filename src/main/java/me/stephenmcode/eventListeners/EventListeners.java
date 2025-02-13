c50338c93410d323a41fd175885cd5c82ddc2728
Stephen <93400341+StephenMCode@users.noreply.github.com>
2025-02-12 18:54:39 -0800

    Created a simple event listener for the PlayerJoinEvent

--- src/main/java/me/stephenmcode/eventListeners/EventListeners.java
+++ src/main/java/me/stephenmcode/eventListeners/EventListeners.java
@@ -1,17 +1,26 @@
 package me.stephenmcode.eventListeners;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class EventListeners extends JavaPlugin {
+public final class EventListeners extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
 
+        System.out.println("StephenMCode's plugin has started");
+
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+
+        System.out.println("A player has joined the server!");
+
     }
+
 }
